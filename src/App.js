269930032878e26d555e03bc1a8f499c15224253
José Carlos <kalarruda@hotmail.com>
2021-06-26 10:03:14 -0300

    adiciona pokedex modif

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+
 import './App.css';
+import React from 'react';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
-  );
+
+class App extends React.Component {
+  
+   App() {
+    return (
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Pokedex pokemons={pokemons} />
+        <button onClick={this.nextPokemon}>NEXT</button>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
